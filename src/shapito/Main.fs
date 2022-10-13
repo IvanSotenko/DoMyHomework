6b2cd1eee94aced33f0ef20b607968bff07868a1
IvanSotenko <eevandeya@gmail.com>
2022-10-13 22:13:49 +0300

    TreeToSet works

--- src/shapito/Main.fs
+++ src/shapito/Main.fs
@@ -1,4 +1,15 @@
 namespace shapito
 
 module Main =
-    printf "Hello :)"
+
+    open MyList
+    open GeneralTree
+
+    [<EntryPoint>]
+    let main argv =
+
+        let test = Node ("a", Cons (Node ("b", Cons (Leaf "d", Cons (Leaf "e", Empty))), Cons (Leaf "c", Empty)))
+
+        printf "%A" (move2 test)
+
+        0
