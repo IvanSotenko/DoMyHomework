4fc3d5ae7016b9905e134521ad9b37580a997bb0
IvanSotenko <eevandeya@gmail.com>
2022-10-13 19:20:00 +0300

    Made a tree with an arbitrary number of children

--- src/shapito/GeneralTree.fs
+++ src/shapito/GeneralTree.fs
@@ -1,2 +1,9 @@
 ﻿module shapito.GeneralTree
 
+open MyList
+
+type GeneralTree<'value> =
+    | Node of value: 'value * children: MyList<GeneralTree<'value>>
+    | Leaf of value: 'value
+
+// let tree = Node (4, Cons (Leaf 3, Cons (Leaf 6, Empty)))
