6bfca2f4c80463f19e5ee3fcecc2d6b23c8f2c71
IvanSotenko <eevandeya@gmail.com>
2022-10-17 13:30:15 +0300

    Cleaned up

--- src/shapito/GeneralTree.fs
+++ src/shapito/GeneralTree.fs
@@ -2,64 +2,38 @@
 
 open MyList
 
-// type GeneralTree<'value> =
-//     | Node of value: Option<'value> * children: MyList<GeneralTree<'value>>
-//     | Leaf of value: 'value
-
- type GeneralTree<'value> =
+type GeneralTree<'value> =
     | Node of value: 'value * children: MyList<GeneralTree<'value>>
     | Leaf of value: 'value
 
- (*
- let rec treeSet tree =
-     match tree with
-     | Leaf v -> Set.empty.Add(v)
-     | Node (Some v, children) ->
-         match children with
-         | Cons (kid, tail) -> (treeSet kid) + Set.empty.Add(v) + (treeSet (Node (Some v, tail)))
-         | Empty -> Set.empty
-*)
+let rec treeToSet tree =
+        match tree with
+            | Leaf v -> Set.empty.Add(v)
+            | Node (v, children) ->
+                    match children with
+                    | Cons (kid, tail) -> (treeToSet kid) + Set.empty.Add(v) + (treeToSet (Node (v, tail)))
+                    | Empty -> Set.empty
 
- //  let rec treeList tree =
- //     match tree with
- //     | Leaf v -> Cons (v, Empty)
- //     | Node (Some v, children) ->
- //         match children with
- //         | Cons (kid, tail) -> concat (concat (Cons (v, Empty)) (treeList kid)) (treeList (Node (Some v, tail)))
- //         | Empty -> Empty
 
- (*
- let rec treeList2 tree =
-     match tree with
-     | Leaf v -> Cons (v, Empty)
+let countDistinct tree = Set.count (treeToSet tree)
 
-     | Node (None, children) ->
-         match children with
-         | Cons (kid, tail) -> concat (treeList2 kid) (treeList2 (Node (None, tail)))
-         | Empty -> Empty
 
-     | Node (Some v, children) ->
-         match children with
-         | Cons (kid, tail) -> concat (concat (Cons (v, Empty)) (treeList2 kid)) (treeList2 (Node (None, tail)))
-         | Empty -> Empty
-*)
+let treeToList tree =
 
- let treeList3 tree =
+    let rec treeToListSub tree isis =
+        match tree, isis with
+        | Leaf v, _ -> Cons (v, Empty)
 
-     let rec treeList3_sub tree isis =
-         match tree, isis with
-         | Leaf v, _ -> Cons (v, Empty)
+        | Node (v, children), false ->
+            match children with
+            | Cons (kid, tail) -> concat (treeToListSub kid true) (treeToListSub (Node (v, tail)) false)
+            | Empty -> Empty
 
-         | Node (v, children), false ->
-             match children with
-             | Cons (kid, tail) -> concat (treeList3_sub kid true) (treeList3_sub (Node (v, tail)) false)
-             | Empty -> Empty
+        | Node (v, children), true ->
+            match children with
+            | Cons (kid, tail) -> concat (concat (Cons (v, Empty)) (treeToListSub kid true)) (treeToListSub (Node (v, tail)) false)
+            | Empty -> Empty
 
-         | Node (v, children), true ->
-             match children with
-             | Cons (kid, tail) -> concat (concat (Cons (v, Empty)) (treeList3_sub kid true)) (treeList3_sub (Node (v, tail)) false)
-             | Empty -> Empty
-
-     treeList3_sub tree true
+    treeToListSub tree true
 
 // let testTree = Node ("a", Cons (Node ("b", Cons (Leaf "d", Cons (Leaf "e", Empty))), Cons (Leaf "c", Empty)))
--- src/shapito/Main.fs
+++ src/shapito/Main.fs
@@ -8,9 +8,11 @@ module Main =
     [<EntryPoint>]
     let main argv =
 
-        // let testTree1 = Node (Some 1, Cons (Node (Some 2, Cons (Leaf 4, Cons (Leaf 5, Empty))), Cons (Leaf 3, Empty)))
+        let testTree1 = Node (1, Cons (Node (2, Cons (Leaf 4, Cons (Leaf 5, Empty))), Cons (Leaf 3, Empty)))
 
-        let testTree2 = Node (1, Cons (Node (2, Cons (Leaf 4, Cons (Leaf 5, Empty))), Cons (Leaf 3, Empty)))
-        printf "%A" (treeList3 testTree2)
+        let testTree2 = Node (1, Cons (Node (2, Cons (Leaf 9, Cons (Leaf 3, Cons (Leaf 0, Cons (Leaf -4, Empty))))), Cons (Leaf 3, Empty)))
+
+        printfn "%A" (treeToList testTree2)
+        printfn "%A" (countDistinct testTree2)
 
         0
