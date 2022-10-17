6e8935a048c222055e758c92da18385c111c4547
IvanSotenko <eevandeya@gmail.com>
2022-10-17 13:01:47 +0300

    Successful attempt to make TreeToLists without using Option

--- src/shapito/GeneralTree.fs
+++ src/shapito/GeneralTree.fs
@@ -2,10 +2,15 @@
 
 open MyList
 
-type GeneralTree<'value> =
-    | Node of value: Option<'value> * children: MyList<GeneralTree<'value>>
+// type GeneralTree<'value> =
+//     | Node of value: Option<'value> * children: MyList<GeneralTree<'value>>
+//     | Leaf of value: 'value
+
+ type GeneralTree<'value> =
+    | Node of value: 'value * children: MyList<GeneralTree<'value>>
     | Leaf of value: 'value
 
+ (*
  let rec treeSet tree =
      match tree with
      | Leaf v -> Set.empty.Add(v)
@@ -13,6 +18,7 @@ type GeneralTree<'value> =
          match children with
          | Cons (kid, tail) -> (treeSet kid) + Set.empty.Add(v) + (treeSet (Node (Some v, tail)))
          | Empty -> Set.empty
+*)
 
  //  let rec treeList tree =
  //     match tree with
@@ -22,6 +28,7 @@ type GeneralTree<'value> =
  //         | Cons (kid, tail) -> concat (concat (Cons (v, Empty)) (treeList kid)) (treeList (Node (Some v, tail)))
  //         | Empty -> Empty
 
+ (*
  let rec treeList2 tree =
      match tree with
      | Leaf v -> Cons (v, Empty)
@@ -35,5 +42,24 @@ type GeneralTree<'value> =
          match children with
          | Cons (kid, tail) -> concat (concat (Cons (v, Empty)) (treeList2 kid)) (treeList2 (Node (None, tail)))
          | Empty -> Empty
+*)
+
+ let treeList3 tree =
+
+     let rec treeList3_sub tree isis =
+         match tree, isis with
+         | Leaf v, _ -> Cons (v, Empty)
+
+         | Node (v, children), false ->
+             match children with
+             | Cons (kid, tail) -> concat (treeList3_sub kid true) (treeList3_sub (Node (v, tail)) false)
+             | Empty -> Empty
+
+         | Node (v, children), true ->
+             match children with
+             | Cons (kid, tail) -> concat (concat (Cons (v, Empty)) (treeList3_sub kid true)) (treeList3_sub (Node (v, tail)) false)
+             | Empty -> Empty
+
+     treeList3_sub tree true
 
 // let testTree = Node ("a", Cons (Node ("b", Cons (Leaf "d", Cons (Leaf "e", Empty))), Cons (Leaf "c", Empty)))
--- src/shapito/Main.fs
+++ src/shapito/Main.fs
@@ -8,8 +8,9 @@ module Main =
     [<EntryPoint>]
     let main argv =
 
-        let testTree = Node (Some 1, Cons (Node (Some 2, Cons (Leaf 4, Cons (Leaf 5, Empty))), Cons (Leaf 3, Empty)))
+        // let testTree1 = Node (Some 1, Cons (Node (Some 2, Cons (Leaf 4, Cons (Leaf 5, Empty))), Cons (Leaf 3, Empty)))
 
-        printf "%A" (treeList2 testTree)
+        let testTree2 = Node (1, Cons (Node (2, Cons (Leaf 4, Cons (Leaf 5, Empty))), Cons (Leaf 3, Empty)))
+        printf "%A" (treeList3 testTree2)
 
         0
