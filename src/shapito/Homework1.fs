3513f4298bed1516664745eb3f8ae08a1e3d37ae
IvanSotenko <eevandeya@gmail.com>
2022-10-27 18:05:48 +0300

    Rolled back changes in hw1 files

--- src/shapito/Homework1.fs
+++ src/shapito/Homework1.fs
@@ -1,63 +1,63 @@
-﻿module DoMyHomework.Homework1
+﻿module shapito.Homework1
 
 // (1) Exponentiation to integer in a simple way using mutable variables
-let sillyPow (bas: float) (power: int) =
+let silly_pow (bas: float) (power: int) =
 
-    let subSillyPow (num: float) (power: int) =
+    let silly_pow_sub (num: float) (power: int) =
 
         let mutable output = 1.
-        let mutable mutPower = power
+        let mutable mut_power = power
 
-        while mutPower > 0 do
+        while mut_power > 0 do
             output <- output * num
-            mutPower <- mutPower - 1
+            mut_power <- mut_power - 1
 
         output
 
     if power < 0 then
-        1. / subSillyPow bas (abs power)
+        1. / silly_pow_sub bas (abs power)
     else
-        subSillyPow bas power
+        silly_pow_sub bas power
 
 
 // (1) Exponentiation to natural and zero power in a simple way by recursion
-let sillyPowRec (bas: float) (power: int) =
+let silly_pow_rec (bas: float) (power: int) =
 
-    let rec subSillyPowRec (bas: float) (power: int) =
+    let rec silly_pow_rec_sub (bas: float) (power: int) =
 
         if power = 0 then
             1.
         elif power = 1 then
             bas
         else
-            bas * subSillyPowRec bas (power - 1)
+            bas * silly_pow_rec_sub bas (power - 1)
 
     if power < 0 then
-        1. / subSillyPowRec bas (abs power)
+        1. / silly_pow_rec_sub bas (abs power)
     else
-        subSillyPowRec bas power
+        silly_pow_rec_sub bas power
 
 
 // (2) Fast power
-let rec fastPow (bas: float) (power: int) =
+let rec fast_pow (bas: float) (power: int) =
 
-    let rec subfastPow (bas: float) (power: int) =
+    let rec fast_pow_sub (bas: float) (power: int) =
         if power = 0 then
             1.
         elif power % 2 = 0 then
-            fastPow (bas * bas) (power / 2)
+            fast_pow (bas * bas) (power / 2)
         else
-            bas * fastPow bas (power - 1)
+            bas * fast_pow bas (power - 1)
 
     if power < 0 then
-        1. / subfastPow bas (abs power)
+        1. / fast_pow_sub bas (abs power)
     else
-        subfastPow bas power
+        fast_pow_sub bas power
 
 
 // (3) The difference between the largest and smallest element of the array
 let delta arr =
-    if Array.isEmpty arr then
+    if arr = [||] then
         failwith "Error in Homework1.delta: empty array was given. Doesn't make sense"
 
     else
@@ -72,7 +72,7 @@ let delta arr =
 
 
 // (4) An array of odd numbers
-let oddNumbersBetween (num1: int) (num2: int) =
+let odd_numbers_between (num1: int) (num2: int) =
 
     let (left_edge: int) =
         if abs (min num1 num2) % 2 = 0 then
