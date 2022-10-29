1cdbe7fcb5e261f328ff04440355f6e92da666b1
IvanSotenko <eevandeya@gmail.com>
2022-10-29 10:28:13 +0300

    QTree prototype

---
+++ src/shapito/QTree.fs
@@ -0,0 +1,6 @@
+﻿module shapito.QTree
+
+type QTree<'Value> =
+    | Node of quadNW: QTree<'Value> * quadNE: QTree<'Value> * quadSW: QTree<'Value> * quadSE: QTree<'Value>
+    | Leaf of value: 'Value
+    | Empty
