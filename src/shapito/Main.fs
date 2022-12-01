6bea5cfd51b9cdb90b9d3616f5fa9cf64fd7be88
IvanSotenko <eevandeya@gmail.com>
2022-12-01 22:12:35 +0300

    Chubby version

--- src/shapito/Main.fs
+++ src/shapito/Main.fs
@@ -20,16 +20,21 @@ module Main =
                                    [3; 5; 3; 9; 2]])
         // printf "%A" test
 
-        let testMatrix = Matrix(array2D [[4; 2; 1; 2]
-                                         [5; 1; 3; 9]
-                                         [5; 1; -2; -1]
-                                         [6; 2; 2; 2]])
+        let m = Matrix(array2D [[4; 2; 1; 2]
+                                [5; 1; 3; 9]
+                                [5; 1; -2; -1]
+                                [6; 2; 2; 2]])
 
-        let testVector = Vector([|1; 1; 1; 5|])
+        let v = Vector([|1; 1; 3; 5|])
 
-        let testVectorMini = Vector([|1; 1|])
-        let testMatrixMini = Matrix(array2D [[1; -3]
-                                             [1; 2]])
+        let E = Matrix(array2D [[1; 0; 0; 0]
+                                [0; 1; 0; 0]
+                                [0; 0; 1; 0]
+                                [0; 0; 0; 1]])
+
+        let v1 = Vector([|1; 1|])
+        let m1 = Matrix(array2D [[1; 1]
+                                 [1; 1]])
 
         // printfn "%A" (naiveMultiply testVector testMatrix (fun a b -> a + b) (fun a b -> a*b)).BinaryTree
         // printfn "%A" (MatXEl testMatrix 5 (*))
@@ -42,8 +47,11 @@ module Main =
                 match tree1, tree2 with
                 | BinTree.Node (l1, r1), BinTree.Node(l2, r2) -> BinTree.Node(treePlus l1 l2, treePlus r1 r2)
                 | BinTree.Leaf a, BinTree.Leaf b -> BinTree.Leaf (plus a b)
-                | BinTree.Empty, BinTree.Empty -> BinTree.Empty
-                | _ -> failwith "oops"
+                | BinTree.Leaf a, BinTree.Node(l, r) -> BinTree.Node(treePlus (BinTree.Leaf a) l, treePlus (BinTree.Leaf a) r)
+                | BinTree.Node(l, r), BinTree.Leaf a -> BinTree.Node(treePlus (BinTree.Leaf a) l, treePlus (BinTree.Leaf a) r)
+                | _ ->
+                    printfn $"tree1 = {tree1}\ntree2 = {tree2}"
+                    failwith "oops"
 
             let rec core btree qtree =
                 match btree, qtree with
@@ -73,7 +81,8 @@ module Main =
             core btree qtree
 
 
-        let res = adventureTime testMatrix testVector (+) (fun a b -> a*b)
+        let res = adventureTime m v (+) (*)
+        let res2 = adventureTime E v (+) (*)
 
         printfn "%A" res
         0
