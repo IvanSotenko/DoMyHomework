d783a0acfed8fd7ba83158a8a0d4e3c7b0304553
IvanSotenko <eevandeya@gmail.com>
2022-12-07 14:10:44 +0300

    Multiply is ready

--- src/shapito/Main.fs
+++ src/shapito/Main.fs
@@ -1,88 +1,53 @@
 namespace DoMyHomework
 
 open shapito
-open QTree
 open Vector
 open Matrix
 open Multiply
-open shapito.BinTree
-open shapito.QTree
 
 module Main =
 
     [<EntryPoint>]
     let main argv =
 
-        let test = Matrix(array2D [[1; 1; 1; 1; 9]
-                                   [1; 1; 1; 1; 5]
-                                   [1; 1; 1; 1; 1]
-                                   [1; 1; 1; 1; 5]
-                                   [3; 5; 3; 9; 2]])
-        // printf "%A" test
+        let M = Matrix(array2D [[Some 1; Some 0; Some 1; Some 3; Some 2]
+                                [Some 2; Some 1; Some -2; Some 3; Some 1]
+                                [Some 3; Some 3; Some 1; Some -5; Some 5]
+                                [Some 3; Some 3; None; Some 1; Some 0]
+                                [None; None; Some 1; Some 4; None]])
 
-        let m = Matrix(array2D [[4; 2; 1; 2]
-                                [5; 1; 3; 9]
-                                [5; 1; -2; -1]
-                                [6; 2; 2; 2]])
+        let M2 = Matrix(array2D [[Some 1; Some 0; Some 1; Some 3; Some 2]
+                                 [Some 2; Some 1; Some -2; Some 3; Some 1]
+                                 [Some 3; Some 3; Some 1; Some -5; Some 5]
+                                 [Some 3; Some 3; Some 2; Some 1; Some 0]
+                                 [Some 1; Some 9; Some 1; Some 4; Some 1]])
 
-        let v = Vector([|1; 1; 3; 5|])
+        let V = Vector([|Some 3; Some 0; Some -4; Some 2; Some 2|])
 
-        let E = Matrix(array2D [[1; 0; 0; 0]
-                                [0; 1; 0; 0]
-                                [0; 0; 1; 0]
-                                [0; 0; 0; 1]])
+        let adjM = array2D [[false; true; true; true; false]
+                            [false; false; false; true; false]
+                            [false; false; false; true; true]
+                            [false; false; false; false; false]
+                            [false; true; false; false; false]]
 
-        let v1 = Vector([|1; 1|])
-        let m1 = Matrix(array2D [[1; 1]
-                                 [1; 1]])
+        let front = [|true; false; false; false; false|]
 
-        // printfn "%A" (naiveMultiply testVector testMatrix (fun a b -> a + b) (fun a b -> a*b)).BinaryTree
-        // printfn "%A" (MatXEl testMatrix 5 (*))
+        let mp a =
+            if a then Some a
+            else None
 
-        let adventureTime (mat: Matrix<'A>) (vec: Vector<'A>) (plus: 'A -> 'A -> 'A) (mult: 'A -> 'A -> 'A) =
-            let btree = vec.BinaryTree
-            let qtree = mat.QuadTree
+        let NoneFront = Array.map mp front
+        let NoneAdjM = Array2D.map mp adjM
 
-            let rec treePlus tree1 tree2 =
-                match tree1, tree2 with
-                | BinTree.Node (l1, r1), BinTree.Node(l2, r2) -> BinTree.Node(treePlus l1 l2, treePlus r1 r2)
-                | BinTree.Leaf a, BinTree.Leaf b -> BinTree.Leaf (plus a b)
-                | BinTree.Leaf a, BinTree.Node(l, r) -> BinTree.Node(treePlus (BinTree.Leaf a) l, treePlus (BinTree.Leaf a) r)
-                | BinTree.Node(l, r), BinTree.Leaf a -> BinTree.Node(treePlus (BinTree.Leaf a) l, treePlus (BinTree.Leaf a) r)
-                | _ ->
-                    printfn $"tree1 = {tree1}\ntree2 = {tree2}"
-                    failwith "oops"
+        let treeAdjMN = Matrix(NoneAdjM)
+        let treeFrontN = Vector(NoneFront)
 
-            let rec core btree qtree =
-                match btree, qtree with
+        let optAdjM = Array2D.map Some adjM
+        let optFront = Array.map Some front
 
-                | BinTree.Node (l, r), Node(nw, ne, sw, se) ->
-                    BinTree.Node(treePlus (core l nw) (core r sw), treePlus (core l ne) (core r se))
+        let treeAdjM = Matrix(optAdjM)
+        let treeFront = Vector(optFront)
 
-                | BinTree.Node (l, r), Leaf a ->
-                    BinTree.Node(treePlus (core l (Leaf a)) (core r (Leaf a)), treePlus (core l (Leaf a)) (core r (Leaf a)))
-
-                | BinTree.Node (l, r), Empty ->
-                    BinTree.Node(treePlus (core l Empty) (core r Empty), treePlus (core l Empty) (core r Empty))
-
-                | BinTree.Leaf a, Node(nw, ne, sw, se) ->
-                    BinTree.Node(treePlus (core (BinTree.Leaf a) nw) (core (BinTree.Leaf a) sw), treePlus (core (BinTree.Leaf a) ne) (core (BinTree.Leaf a) se))
-
-                | BinTree.Leaf a, Leaf b -> BinTree.Leaf (mult a b)
-
-                | BinTree.Leaf _, Empty -> BinTree.Empty
-
-                | BinTree.Empty, Node _ -> BinTree.Empty
-
-                | BinTree.Empty, Leaf _ -> BinTree.Empty
-
-                | BinTree.Empty, Empty -> BinTree.Empty
-
-            core btree qtree
-
-
-        let res = adventureTime m v (+) (*)
-        let res2 = adventureTime E v (+) (*)
-
-        printfn "%A" res
+        // printfn "%A\n" V.Data
+        printfn "%A" (naiveMultiply treeFrontN treeAdjMN (||) (&&)).Data
         0
--- src/shapito/Multiply.fs
+++ src/shapito/Multiply.fs
@@ -4,37 +4,35 @@ open Vector
 open BinTree
 open QTree
 
-// let naiveMultiply (vec: Vector<'A>) (mat: Matrix<'A>) (plus: 'A -> 'A -> 'A) (mult: 'A -> 'A -> 'A) =
-//
-//     if vec.Length <> mat.Length1 then failwith $"The dimensions of the matrix are incompatible
-//                                                  for multiplication with the dimensions of the vector:
-//                                                  vector length is {vec.Length} but matrix size is {mat.Length1}x{mat.Length2}"
-//     else
-//         let columns = mat.actualLen2
-//         let len = vec.actualLength
-//
-//         let mutable newVector = [||]
-//         for i in 0 .. (columns - 1) do
-//
-//             let m1, m2 =
-//                 match (vec.getItem 0), (mat.getItem (0, i)) with
-//                 | Some a, Some b -> a, b
-//                 | _ -> failwith "some exception"
-//
-//             let mutable el = multiply m1 m2
-//
-//             for j in 1 .. (len - 1) do
-//
-//                 let m1, m2 =
-//                     match (vec.getItem j), (mat.getItem (j, i)) with
-//                     | Some a, Some b -> a, b
-//                     | _ -> failwith "some exception"
-//
-//                 el <- add el (multiply m1 m2)
-//
-//             newVector <- Array.append newVector [|el|]
-//
-//         Vector(newVector)
+let naiveMultiply (vec: Vector<'A>) (mat: Matrix<'A>) (add: 'A -> 'A -> 'A) (multiply: 'A -> 'A -> 'A) =
+
+    let plus a b =
+        match a, b with
+        | Some x, Some y -> Some (add x y)
+        | Some x, None -> Some x
+        | None, Some x -> Some x
+        | None, None -> None
+
+    let multy a b =
+        match a, b with
+        | Some x, Some y -> Some (add x y)
+        | _ -> None
+
+    if vec.Length <> mat.Length1 then failwith $"The dimensions of the matrix are incompatible
+                                                 for multiplication with the dimensions of the vector:
+                                                 vector length is {vec.Length} but matrix size is {mat.Length1}x{mat.Length2}"
+    else
+        let columns = mat.Length2
+        let len = vec.Length
+
+        let mutable newVector = [||]
+        for i in 0 .. (columns - 1) do
+            let mutable el = multy (vec.getItem 0) (mat.getItem (0, i))
+            for j in 1 .. (len - 1) do
+                el <- plus el (multy (vec.getItem j) (mat.getItem (j, i)))
+            newVector <- Array.append newVector [|el|]
+
+        Vector(newVector)
 
 
 let addTree tree1 tree2 addFunc =
@@ -42,15 +40,15 @@ let addTree tree1 tree2 addFunc =
     let rec treePlusSub tree1 tree2 =
         match tree1, tree2 with
         | BinTree.Node (l1, r1), BinTree.Node(l2, r2) -> BinTree.Node(treePlusSub l1 l2, treePlusSub r1 r2)
-        | BinTree.Node (l, r), tree -> BinTree.Node(treePlusSub l tree, treePlusSub r tree)
-        | tree, BinTree.Node (l, r) -> BinTree.Node(treePlusSub l tree, treePlusSub r tree)
+        | BinTree.Node (l, r), leafOrEmpty -> BinTree.Node(treePlusSub l leafOrEmpty, treePlusSub r leafOrEmpty)
+        | leafOrEmpty, BinTree.Node (l, r) -> BinTree.Node(treePlusSub l leafOrEmpty, treePlusSub r leafOrEmpty)
         | BinTree.Leaf a, BinTree.Leaf b -> BinTree.Leaf (addFunc a b)
         | leafOrEmpty, BinTree.Empty -> leafOrEmpty
         | BinTree.Empty, leafOrEmpty -> leafOrEmpty
 
     treePlusSub tree1 tree2
 
-let multiply (vec: Vector<'A>) (mat: Matrix<'A>) (plus: 'A -> 'A -> 'A) (mult: 'A -> 'A -> 'A) =
+let multiply (vec: Vector<'A>) (mat: Matrix<'A>) (add: 'A -> 'A -> 'A) (mult: 'A -> 'A -> 'A) =
 
     if vec.Length <> mat.Length1 then failwith $"The dimensions of the matrix are incompatible
                                                  for multiplication with the dimensions of the vector:
@@ -59,19 +57,17 @@ let multiply (vec: Vector<'A>) (mat: Matrix<'A>) (plus: 'A -> 'A -> 'A) (mult: '
     let bTree = vec.Data
     let qTree = mat.Data
 
-    printfn "%A\n \n%A" bTree qTree
-
     let rec core bTree qTree =
         match bTree, qTree with
 
         | BinTree.Node (l, r), Node(nw, ne, sw, se) ->
-            BinTree.Node(addTree (core l nw) (core r sw) plus, addTree (core l ne) (core r se) plus)
+            BinTree.Node(addTree (core l nw) (core r sw) add, addTree (core l ne) (core r se) add)
 
         | BinTree.Node (l, r), qTree ->
-            BinTree.Node(addTree (core l qTree) (core r qTree) plus, addTree (core l qTree) (core r qTree) plus)
+            BinTree.Node(addTree (core l qTree) (core r qTree) add, addTree (core l qTree) (core r qTree) add)
 
         | btree, Node(nw, ne, sw, se) ->
-            BinTree.Node(addTree (core btree nw) (core btree sw) plus, addTree (core btree ne) (core btree se) plus)
+            BinTree.Node(addTree (core btree nw) (core btree sw) add, addTree (core btree ne) (core btree se) add)
 
         | BinTree.Leaf a, Leaf b -> BinTree.Leaf (mult a b)
 
