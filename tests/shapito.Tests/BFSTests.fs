b1d321c66908892e9af041d8e361ba44510538a9
IvanSotenko <eevandeya@gmail.com>
2022-12-25 21:12:45 +0300

    Added tests for BFS

--- tests/shapito.Tests/BFSTests.fs
+++ tests/shapito.Tests/BFSTests.fs
@@ -8,10 +8,38 @@ open FsCheck
 open testMatrix
 open Matrix
 open BFS
+open Vector
 
 
-module randomVertsGeneration =
+module randomGenerations =
     let rnd = Random()
+
+    let genRandomArray n =
+        Array.init n (fun _ -> Some(rnd.Next(-5, 5)))
+
+    let genRandomNoneArray n =
+        Array.init n (fun _ ->
+            if rnd.Next(1, 5) = 4 then
+                None
+            else
+                Some(rnd.Next(-5, 5)))
+
+    let genRandomVector n = Vector(genRandomArray n)
+    let genRandomNoneVector n = Vector(genRandomNoneArray n)
+
+
+    let genRandomArray2D x y =
+        Array2D.init x y (fun _ _ -> Some(rnd.Next(-5, 5)))
+
+    let genRandomNoneArray2D x y =
+        Array2D.init x y (fun _ _ ->
+            if rnd.Next(1, 5) = 4 then
+                None
+            else
+                Some(rnd.Next(-5, 5)))
+
+    let genRandomMatrix x y = Matrix(genRandomArray2D x y)
+    let genRandomNoneMatrix x y = Matrix(genRandomNoneArray2D x y)
 
     let randomVerts n =
 
@@ -21,11 +49,11 @@ module randomVertsGeneration =
         let transfer (listOut: list<uint>) (listIn: list<uint>) =
             let ind = rnd.Next(listOut.Length)
             let el = listOut[ind]
-
             (List.removeAt ind listOut), (List.append listIn [el])
 
         let rec generator (listDon: uint list) (listRec: uint list) =
-            if rnd.Next(1,7) = 1 || listDon.IsEmpty then
+            // Setting the possible length of the list here
+            if rnd.Next(1,17) = 1 || listDon.IsEmpty then
                 listRec
             else
                 let newListDon, newListRec = transfer listDon listRec
@@ -34,25 +62,64 @@ module randomVertsGeneration =
         let newInit, newLst = transfer initList lst
         generator newInit newLst
 
+let testMat1 = Matrix(testMatrix1, 115, 115)
+let testMat2 = Matrix(testMatrix2, 132, 132)
+let testMat3 = Matrix(testMatrix3, 100, 100)
 
-let testMat = Matrix(bigM, 115, 115)
-
-open randomVertsGeneration
+open randomGenerations
 
 [<Tests>]
-let t =
+let BFSTest =
     testList
-        "blah"
+        "Tests for BFS.BFS function"
         [
-         testProperty ""
+         testProperty "Random starting vertices for a single graph (football)"
          <| fun _ ->
 
              let verts = randomVerts 115u
-             // let verts = [uint (rnd.Next(1, 115))]
 
-             let expectedResult = (naiveBFS verts testMat).Data
-             let actualResult = (BFS verts testMat).Data
+             let expectedResult = (naiveBFS verts testMat1).Data
+             let actualResult = (BFS verts testMat1).Data
 
-             Expect.equal actualResult expectedResult $"The results were different, startVerts = {verts}"]
+             Expect.equal actualResult expectedResult $"The results were different, startVerts = {verts}"
 
+         testProperty "Random starting vertices for a single graph (lnsp_131)"
+         <| fun _ ->
 
+             let verts = randomVerts 132u
+
+             let expectedResult = (naiveBFS verts testMat2).Data
+             let actualResult = (BFS verts testMat2).Data
+
+             Expect.equal actualResult expectedResult $"The results were different, startVerts = {verts}"
+
+         testProperty "Random starting vertices for a single graph (tub100)"
+         <| fun _ ->
+
+             let verts = randomVerts 100u
+
+             let expectedResult = (naiveBFS verts testMat3).Data
+             let actualResult = (BFS verts testMat3).Data
+
+             Expect.equal actualResult expectedResult $"The results were different, startVerts = {verts}"
+
+         testCase "With an empty list of starting vertices the result should be all None"
+         <| fun _ ->
+
+             let verts = []
+             let expectedResult = BinTree.Empty
+             let actualResult = (BFS verts testMat3).Data
+
+             Expect.equal actualResult expectedResult "The results were different"
+
+         testProperty "With an empty graph the result should be all None"
+         <| fun _ ->
+
+             let verts = randomVerts 100u
+             let mat = Matrix(QTree.Empty, 100, 100)
+
+             let expectedResult = BinTree.Empty
+             let actualResult = (BFS verts mat).Data
+
+             Expect.equal actualResult expectedResult "The results were different"
+             ]
